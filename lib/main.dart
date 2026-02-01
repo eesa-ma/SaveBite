fdf99d8154141603dfeea1fde83875567a6e9f48
eesa-ma <eesama30april04@gmail.com>
2026-02-01 18:30:58 +0530

    Initial Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:save_bite/firebase_options.dart';
 
-void main() {
+void main() async {
+  //Firebase initialisation
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
