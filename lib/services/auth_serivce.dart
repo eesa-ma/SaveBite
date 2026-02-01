dd086fedbb2a04ccf0c98eed2ba716c7ad203d42
eesa-ma <eesama30april04@gmail.com>
2026-02-01 20:48:41 +0530

    directory setup

---
+++ lib/services/auth_serivce.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign Up
+  Future<User?> signUp(String email, String password) async {
+    final credential = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return credential.user;
+  }
+
+  // Login
+  Future<User?> login(String email, String password) async {
+    final credential = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return credential.user;
+  }
+
+  // Logout
+  Future<void> logout() async {
+    await _auth.signOut();
+  }
+
+  // Current User
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+}
